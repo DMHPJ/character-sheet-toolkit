a1a71c619a3661bb23c71ae35d66eec8681da611
DMHPJ <c2020625277@hotmail.com>
2026-04-11 15:13:58 +0800

    github page配置

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // 静态站点
+  output: "export",
+  trailingSlash: true,
+  basePath: isGithubPages ? "/character-sheet-toolkit" : "",
+  assetPrefix: isGithubPages ? "/character-sheet-toolkit/" : undefined,
+  images: {
+    unoptimized: true,
+  },
   ...(process.env.BUILD_DIST_DIR ? { distDir: process.env.BUILD_DIST_DIR } : {}),
 };
 
